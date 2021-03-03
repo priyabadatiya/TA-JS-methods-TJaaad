c11838c888b442905c2c8266274b94f54600f6f7
priyabadatiya <priyabadatiya@gmail.com>
2021-03-04 01:02:26 +0530

    refactor

--- block-BJaadh/code/index.js
+++ block-BJaadh/code/index.js
@@ -62,7 +62,7 @@ console.log(strings.unshift("New Word"));
 console.log(numbers.slice(3, 7));
 
 // - Make a subset of strings array ['a','collection']
-console.log(strings).slice(2, 4)
+console.log(strings.slice(3, 5))
 
 // - Replace 12 & 18 with 1221 and 1881
 numbers.map((v) => {
@@ -76,9 +76,13 @@ numbers.map((v) => {
 });
 
 // - Replace words in strings array with the length of the word
-
+let stringslength = strings.map((string) => string.length)
 
 // - Find the sum of the length of words using above question
+stringslength.reduce((acc, cb) => {
+    acc = acc + cb
+    return acc;
+}, 0)
 
 // - Customers Array
 var customers = [
@@ -91,7 +95,7 @@ var customers = [
 console.log(customers.filter((customer) => customer.firstname.startsWith("J")));
 
 // - Create new array with only first name
-
+let firstNameCustomer = customers.map((customer) => customer.firstName);
 
 // - Create new array with all the full names (ex: "Joe Blogs")
 let fullName = customers.map((customer) => { `${customer.firstname} ${customer.lastname}` });
@@ -100,3 +104,15 @@ let fullName = customers.map((customer) => { `${customer.firstname} ${customer.l
 console.log([...fullName].sort());
 
 // - Create a new array that contains only user who has at least one vowel in the firstname.
+let vowelCustomer = customers.filter((customer) => {
+    if (customer.firstname.toLowerCase().includes("a") ||
+        customer.firstname.toLowerCase().includes("e") ||
+        customer.firstname.toLowerCase().includes("i") ||
+        customer.firstname.toLowerCase().includes("o") ||
+        customer.firstname.toLowerCase().includes("u")
+    ) {
+        return true;
+    } else {
+        return false
+    }
+});
